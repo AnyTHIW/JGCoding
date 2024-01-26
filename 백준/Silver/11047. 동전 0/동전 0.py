629cbb32ea11c0428f435b4e66b9afc24e4bd000
LSL <79294133+AnyTHIW@users.noreply.github.com>
2024-01-26 15:08:33 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -8,9 +8,10 @@ _A_value = [ int(INPUT_FROM_SYS().strip()) for _ in range(_N)]
 
 count_coins = 0
 
-for item in reversed(_A_value):
-    count_item = _K // item
-    _K = _K % int(item)
+# for item in reversed(_A_value):
+for i in range(len(_A_value)-1, -1, -1):
+    count_item = _K // _A_value[i]
+    _K = _K % _A_value[i]
     count_coins += count_item
     
 print(count_coins)
