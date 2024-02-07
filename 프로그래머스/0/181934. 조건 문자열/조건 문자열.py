1d507106cf88462be43235f721acb76adc145975
LSL <79294133+AnyTHIW@users.noreply.github.com>
2024-02-07 16:13:32 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,10 @@
+def solution(ineq, eq, n, m):
+    if eq == '=' and n == m:
+        return 1
+    
+    if ineq == '>' and n > m:
+        return 1
+    elif ineq == '<' and n < m:
+        return 1
+    
+    return 0
