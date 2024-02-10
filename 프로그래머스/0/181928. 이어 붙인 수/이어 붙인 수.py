b59556c7b476d620ff9e4d4dc9b01ee985710176
LSL <79294133+AnyTHIW@users.noreply.github.com>
2024-02-10 15:32:22 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    odd_string = ""
+    even_string = ""
+    
+    for item in num_list:
+        if item%2:
+            odd_string += str(item)
+        else:
+            even_string += str(item)
+            
+    answer = int(odd_string) + int(even_string)
+    return answer
