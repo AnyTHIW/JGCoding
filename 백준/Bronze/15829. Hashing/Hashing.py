599f6db5d884b5b2c436583adef8489a0246add3
LSL <79294133+AnyTHIW@users.noreply.github.com>
2024-03-09 10:14:07 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,8 +1,12 @@
 import sys
 IFS = sys.stdin.readline
+_M = 1234567891
 
 _N = int(IFS().rstrip())
 _String = [*IFS().rstrip()]
+
+
+# print(_N, _String, sep="  ")
 
 count = 0
 toTargetNumber = 0
@@ -12,5 +16,16 @@ for item in _String:
     toIntNumber = ord(item) - ord('a') + 1
     toTargetNumber += toIntNumber * base
     
+    # print('==========')
+    # print(base)
+    # print(ord(item))
+    # print(ord('a'))
+    # print(toIntNumber)
+    # print(toTargetNumber)
+    
     count += 1
+# print(toTargetNumber)
+
+toTargetNumber = toTargetNumber % _M
+
 print(toTargetNumber)
