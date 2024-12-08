4a45e5876ef59a95d265eab190f5c3861bd3e0c5
SL <79294133+AnyTHIW@users.noreply.github.com>
2024-12-08 20:06:17 +0900

    [Silver II] Title: 나무 자르기, Time: 1376 ms, Memory: 148224 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -5,23 +5,32 @@ input = sys.stdin.readline
 _N, _M = map(int, input().split())
 _TreeList = list(map(int, input().split()))
 
-heightMin = 0
-heightMax = max(_TreeList)
-
-l = heightMin
-r = heightMax
-
-tempHeight = 0
-
-while l <= r:
-    m = (l + r) // 2
-    sumLog = sum(item - m for item in _TreeList if item > m)
-        
-    if sumLog >= _M:
-        tempHeight = m
-        l = m + 1
-
-    elif sumLog < _M:
-        r = m - 1
+def BS_rec():
+    l = 0
+    r = max(_TreeList)
     
-print(tempHeight)
+    def BS_rec_inner(l, r):
+        if l > r:
+            return r
+        
+        m = (l + r) // 2
+        
+        sumLog = 0
+        for item in _TreeList:
+            if m > item:
+                continue
+            
+            sumLog += item - m
+            if sumLog > _M:
+                break
+            
+        if sumLog >= _M:
+            return BS_rec_inner(m + 1, r)
+
+        elif sumLog < _M:
+            return BS_rec_inner(l, m - 1)
+        
+    return BS_rec_inner(l, r)
+    
+print(BS_rec())
+    
