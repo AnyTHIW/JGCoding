7b7fe96868a61b0b36d186699165f0187099a913
SL <79294133+AnyTHIW@users.noreply.github.com>
2024-12-18 22:46:42 +0900

    [Silver I] Title: 숨바꼭질, Time: 144 ms, Memory: 44900 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -8,25 +8,29 @@ _M, _K = map(int, input().split())
 dX = [0, -1, 1]
 dMul = 2
 
-Q = deque()
-Q.append(_M)
-minTimeDct = {_M: 0}
-
-while Q:
-    tempPos = Q.popleft()
-        
-    if tempPos == _K:
-        break
+if _M < _K:
+    Q = deque()
+    Q.append(_M)
+    minTimeDct = {_M: 0}
     
-    for dir in dX:
-        if dir == 0:
-            newPos = tempPos * dMul
-        else:
-            newPos = tempPos + dir
+    while Q:
+        tempPos = Q.popleft()
             
-        if 0 <= newPos <= 100000:
-            if newPos not in minTimeDct:
-                minTimeDct[newPos] = minTimeDct[tempPos] + 1
-                Q.append(newPos)
-            
-print(minTimeDct[_K])
+        if tempPos == _K:
+            break
+        
+        for dir in dX:
+            if dir == 0:
+                newPos = tempPos * dMul
+            else:
+                newPos = tempPos + dir
+                
+            if 0 <= newPos <= 100000:
+                if newPos not in minTimeDct:
+                    minTimeDct[newPos] = minTimeDct[tempPos] + 1
+                    Q.append(newPos)
+                
+    print(minTimeDct[_K])
+
+else:
+    print(_M - _K)
