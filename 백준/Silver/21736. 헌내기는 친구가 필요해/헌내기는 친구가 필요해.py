81851386987398a19f8f5286235a35b2460ddd6f
SL <79294133+AnyTHIW@users.noreply.github.com>
2025-01-13 08:46:24 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 336 ms, Memory: 39008 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -10,43 +10,32 @@ def BFS():
     dY, dX = [1, -1, 0, 0], [0, 0, -1, 1]
     Q = deque()
     
-    xIdx = 0
-    yIdx = 0
-    breaker = False
-    for string in CampusMap_:
-        for char in string:
-            if char == "I":
-                breaker = True
-                break
-            xIdx += 1
-            
-        if (breaker):
+    for yIdx, string in enumerate(CampusMap_):
+        if "I" in string:
+            xIdx = string.index("I")
+            Q.append((xIdx, yIdx))
             break
-        xIdx = 0
-        yIdx += 1
+            
+    vst = [[False]*M_ for _ in range(N_)]
+    vst[yIdx][xIdx] = True
     
-    vst = {}
     person = 0
-    Q.append((xIdx, yIdx))
-    vst[(xIdx, yIdx)] = True
     
     while len(Q):
-        tempPos = Q.popleft()
+        currX, currY = Q.popleft()
     
         for idx in range(4):
-            newXPos, newYPos = tempPos[0] + dX[idx], tempPos[1] + dY[idx]
-            if 0 <= newXPos < M_ and 0 <= newYPos < N_ and not (newXPos, newYPos) in vst:
+            newXPos, newYPos = currX + dX[idx], currY + dY[idx]
+            if 0 <= newXPos < M_ and 0 <= newYPos < N_ and not vst[newYPos][newXPos]:
+                vst[newYPos][newXPos] = True
                 search = CampusMap_[newYPos][newXPos]
+                
                 if search == "X":
                     continue
 
                 if search == "P":
                     person += 1
                     
-                elif search == "O":
-                    pass
-                
-                vst[(newXPos, newYPos)] = True
                 Q.append((newXPos, newYPos))
                     
     return print(person) if person != 0 else print("TT")
