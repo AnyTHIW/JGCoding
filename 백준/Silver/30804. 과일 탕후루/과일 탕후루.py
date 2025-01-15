dd8afee41b288832130a644e80c79d0f72752c03
SL <79294133+AnyTHIW@users.noreply.github.com>
2025-01-15 23:16:57 +0900

    [Silver II] Title: 과일 탕후루, Time: 196 ms, Memory: 35588 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
@@ -6,29 +6,27 @@ N_ = int(input().rstrip())
 SkewerList_ = list(map(int, input().split()))
 
 lPointer, rPointer = 0, 0
-typeFreq = [0] * 10
-typeList = set()
+typeFreq = [-1] + [0] * 9
+typeNumber = 0
 maxLength = 0
 
 while (rPointer < N_):
     extendItem = SkewerList_[rPointer]
     typeFreq[extendItem] += 1
     
-    if extendItem not in typeList:
-        typeList.add(extendItem)
+    if typeFreq[extendItem] == 1:
+        typeNumber += 1
     
-    while (len(typeList) > 2):
+    while (typeNumber > 2):
         shortenItem = SkewerList_[lPointer]
         typeFreq[shortenItem] -= 1
         
         if typeFreq[shortenItem] == 0:
-            typeList.remove(shortenItem)
+            typeNumber -= 1
             
         lPointer += 1
     
-    if maxLength < (rPointer - lPointer + 1):
-        maxLength = rPointer - lPointer + 1
-        
+    maxLength = max(maxLength, rPointer - lPointer + 1)
     rPointer += 1
 
 print(maxLength)
