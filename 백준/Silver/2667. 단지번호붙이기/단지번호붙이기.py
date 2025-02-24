665b20a67d521d9c5bf000b66186ad3edfca07e9
SL <79294133+AnyTHIW@users.noreply.github.com>
2025-02-24 09:59:06 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -7,49 +7,40 @@ inputs = sys.stdin.readlines
 N_ = int(input().rstrip())
 listN_ = [[int(_)  for _ in line.rstrip()] for line in inputs()]
 
-# print(N_)
-# print(listN_)
-# print(type(listN_[0][0]))
-
 vst = [[False for _ in range(N_)] for _ in range(N_)]
 
 result = []
 cntComplex = 0
 
-def BFS(start:tuple):
+def BFS(startX, startY):
   # 상하좌우
   dY = [1, -1, 0, 0]
   dX = [0, 0, 1, -1]
 
-  Q = deque([start])
-  vst[start[0]][start[1]] = True
+  Q = deque([(startX, startY)])
+  vst[startX][startY] = True
+  cntBuilding = 1
 
-  def BFS_Aux():
-    cntBuilding = 1
+  while Q:
+    currX, currY= Q.popleft()
 
-    while Q:
-      currPos = Q.popleft()
-      currX, currY = currPos[0], currPos[1]
+    for i in range(4):
+      nextX, nextY = currX + dX[i], currY + dY[i]
 
-      for i in range(4):
-        nextX, nextY = currX + dX[i], currY + dY[i]
-        
-        if 0 <= nextX < N_ and 0 <= nextY < N_ and listN_[nextX][nextY] == 1 and not vst[nextX][nextY]:
-          nextPos = (nextX, nextY)
+      if 0 <= nextX < N_ and 0 <= nextY < N_ and listN_[nextX][nextY] == 1 and not vst[nextX][nextY]:
+        nextPos = (nextX, nextY)
 
-          vst[nextX][nextY] = True
-          cntBuilding += 1
-          Q.append(nextPos)
+        vst[nextX][nextY] = True
+        cntBuilding += 1
+        Q.append(nextPos)
 
-    return result.append(cntBuilding)
-  return BFS_Aux()
+  result.append(cntBuilding)
 
 for i in range(N_):
   for j in range(N_):
     if listN_[i][j] == 1 and not vst[i][j]:
       cntComplex += 1
-      BFS((i, j))
+      BFS(i, j)
 
 print(cntComplex)
-result.sort()
-print(*result, sep = '\n')
+print(*sorted(result), sep='\n')
