ac22e642cb9f1165077352a455885e55f5e1c305
AnyTHIW <79294133+AnyTHIW@users.noreply.github.com>
2025-10-08 21:53:06 +0900

    [level 1] Title: 삼총사, Time: 0.11 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,10 +1,10 @@
 def solution(number):
-    answer = []
+    answer = 0
     
     for i in range(len(number)-2):
         for j in range(i+1,len(number)-1):
             for k in range(j+1,len(number)):
                 if (number[i] + number[j] + number[k] == 0):
-                    answer.append((i, j, k))
+                    answer += 1
     
-    return len(answer)
+    return answer
