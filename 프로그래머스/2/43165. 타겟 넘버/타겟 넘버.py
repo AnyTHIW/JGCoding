0b0eb3124e4a92ae7ba487357be53785f51377b2
AnyTHIW <79294133+AnyTHIW@users.noreply.github.com>
2025-10-10 16:40:53 +0900

    [level 2] Title: 타겟 넘버, Time: 358.39 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,19 +1,18 @@
 def solution(numbers, target):
-    perfect_count = 0
-    curr = 0
-    result = 0
+    method = 0
     
-    def addOrSubtract(rst, crr):
-        nonlocal perfect_count
+    def addOrSubtract(sumResult, idx):
+        nonlocal method
         
-        if crr == len(numbers):
-            if rst == target:
-                perfect_count += 1
+        if idx == len(numbers):
+            
+            if sumResult == target:
+                method += 1
             return
 
-        addOrSubtract(rst + numbers[crr], crr + 1)
-        addOrSubtract(rst - numbers[crr], crr + 1)
+        addOrSubtract(sumResult + numbers[idx], idx + 1)
+        addOrSubtract(sumResult - numbers[idx], idx + 1)
 
     addOrSubtract(0, 0)
 
-    return perfect_count
+    return method
